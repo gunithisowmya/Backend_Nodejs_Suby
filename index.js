6aae904ce8fc3ea8eb06c8b18188fe3a55ab413b
gunithisowmya <sowmyagunithi790@gmail.com>
2025-03-13 21:14:44 +0530

    first time pushing my project to git

---
+++ index.js
@@ -0,0 +1,41 @@
+const dotEnv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const vendorRoutes = require("./routes/vendorRoutes");
+const firmRoutes = require("./routes/firmRoutes");
+const productRoutes = require('./routes/productRoutes');
+//const cors=require('cors');
+const path=require('path');
+
+dotEnv.config();
+
+const app = express();
+const PORT = 4000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected successfully"))
+    .catch((error) => console.log(error));
+
+// Middleware
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// ✅ Define /home route first
+app.use('/home', (req, res) => {
+    res.send("<h1>Welcome to SUBY</h1>");
+});
+
+// API routes
+app.use('/vendor', vendorRoutes);
+app.use('/firm', firmRoutes);
+app.use('/product', productRoutes);
+app.use('/uploads',express.static('uploads'));
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server started and running at ${PORT}`);
+});
